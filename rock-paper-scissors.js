fc524116ecdb6604cf94b295a298c35b2fbd7a7b
higgsboson909 <higgsbosson906@gmail.com>
2024-09-12 14:15:30 +0500

    Remove breaks from switch

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,27 +4,27 @@ function getComputerChoice(){
     switch(randValue){
         case 0:
             return "rock";
-            break;
         case 1:
             return "paper";
-            break;
         case 2:
             return "scissors";
-            break;
     }
 }
 
 // return human choice
 function getHumanChoice(){
-    let choice = prompt("- Rock\n- Paper\n- Scissors\nEnter your choice : ");
-    choice = choice.toLowerCase();                                                      // modify player input to lowercase
-    if(choice === "rock" || choice === "paper" || choice === "scissors"){               // user choice validation
-        return choice;
-    }
-    else{
-        alert("You have entered wrong choice!\nTry Again");
-        return getHumanChoice();
-    }
+    let choice;
+    
+    // for input validation
+    do{
+        choice = prompt("- Rock\n- Paper\n- Scissors\nEnter your choice : ");
+        choice = choice.toLowerCase();                          // modify player input to lowercase
+        if(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+            alert("Invalid Choice!\nTry Again...")
+        }                                            
+    }while(choice !== "rock" && choice !== "paper" && choice !== "scissors");
+
+    return choice;          // return choice if input is valid
 }
 
 // store score of both players
